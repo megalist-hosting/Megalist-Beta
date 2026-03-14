3361255c841e2a3f07be27787f5bcb5899d94b39
DoggoDEV <tetopearofficial@gmail.com>
2026-03-14 19:24:34 -0300

    old

---
+++ js/score.js
@@ -0,0 +1,46 @@
+/**
+ * Numbers of decimal digits to round to
+ */
+const scale = 3;
+
+/**
+ * Calculate the score awarded when having a certain percentage on a list level
+ * @param {Number} rank Position on the list
+ * @param {Number} percent Percentage of completion
+ * @param {Number} minPercent Minimum percentage required
+ * @returns {Number}
+ */
+
+export function score(rank, percent, minPercent) {
+    const pontosTopo = 350;
+    const diferenca = 18.29;
+
+    let base = pontosTopo - (rank - 1) * diferenca;
+
+    let score = base * ((percent - (minPercent - 1)) / (100 - (minPercent - 1)));
+
+    score = Math.max(0, score);
+
+    if (percent != 100) {
+        return round(score - score / 3);
+    }
+
+    return Math.max(round(score), 0);
+}
+
+export function round(num) {
+    if (!('' + num).includes('e')) {
+        return +(Math.round(num + 'e+' + scale) + 'e-' + scale);
+    } else {
+        var arr = ('' + num).split('e');
+        var sig = '';
+        if (+arr[1] + scale > 0) {
+            sig = '+';
+        }
+        return +(
+            Math.round(+arr[0] + 'e' + sig + (+arr[1] + scale)) +
+            'e-' +
+            scale
+        );
+    }
+}
